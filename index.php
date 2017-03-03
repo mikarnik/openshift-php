4e0c172b741b525ad3cc1c2ff1049609edd524c8
mikarnik <michal.karnik@gmail.com>
2017-03-03 14:22:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
   <title>OpenShift Test</title>
  </head>
 <body>
-
+<h1>
 <?php
 echo "Hello from the server: ";
 echo gethostname() . "<br>";
 echo "----2-<br>";
 echo "Today is " . date("Y.m.d") . "<br>";
 ?>
-
+</h1>
 </body>
 </html>
